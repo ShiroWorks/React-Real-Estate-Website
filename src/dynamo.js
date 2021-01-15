012df6b23f464f3608f33c0029c7da82cef60d96
arya burke <aryaburke4@gmail.com>
2021-01-15 11:40:41 -0500

    fixed aws credentials

--- src/dynamo.js
+++ src/dynamo.js
@@ -1,32 +1,29 @@
-import * as AWS from 'aws-sdk';
+var AWS = require('aws-sdk');
 export default class Dynamo {
-    constructor() {
-        AWS.config.region = process.env.REACT_APP_AWS_REGION; // Region
-        AWS.config.credentials = new AWS.Credentials({
-            accessKeyID: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
-        });
-        this.dynamodb = new AWS.DynamoDB();
-        console.log(this.dynamodb.endpoint);
-        this.docClient = new AWS.DynamoDB.DocumentClient();
-    };
+    
+    constructor() {
+        AWS.config.update({accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+          region: process.env.REACT_APP_AWS_REGION,});
+    };
 
-    queryData() {
-        //takes a 2-character state code and queries the data for it
-        var params = {
-            TableName : "properties",
-            Key: {
-                'zpid': '109181542'
-            }
-        };
-        this.docClient.scan(params, function (err, data) {
-            if (err) {
-                console.log(err, err.stack);
-                return {};
-            } else {
-                console.log('data');
-                return data.Items;
-            }
-        });
-    }
+    queryData() {
+        //takes a 2-character state code and queries the data for it
+        var dynamodb = new AWS.DynamoDB();
+
+        this.docClient = new AWS.DynamoDB.DocumentClient();
+
+        var params = {
+            TableName : "properties"
+        };
+        dynamodb.scan(params, function (err, data) {
+            if (err) {
+                console.log(err, err.stack);
+                return {}
+            } else {
+                console.log('data', data);
+                return data.Items;
+            }
+        });
+    }
 };
