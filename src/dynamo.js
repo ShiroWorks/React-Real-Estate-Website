a208cdb047718dc6aa55ad90990e1dfbdb359f09
Michael Nathanson <michael.nathanson@gmail.com>
2021-01-19 14:20:57 -0500

    Revert "fixed AWS credentials"
    This reverts commit 6b47708bc91cf634dd958ef1b45402e20918e56a.

--- src/dynamo.js
+++ src/dynamo.js
@@ -1,30 +1,31 @@
-var AWS = require('aws-sdk');
+import * as AWS from 'aws-sdk';
 export default class Dynamo {
     constructor() {
-       
-        AWS.config.update({accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-          region: process.env.REACT_APP_AWS_REGION,});
-
-        
+        AWS.config.region = process.env.REACT_APP_AWS_REGION; // Region
+        AWS.config.credentials = new AWS.Credentials({
+            accessKeyID: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+        });
+        AWS.config.credentials.accessKeyID = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
+        AWS.config.credentials.secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+        this.dynamodb = new AWS.DynamoDB();
+        this.docClient = new AWS.DynamoDB.DocumentClient();
     };
 
     queryData() {
         //takes a 2-character state code and queries the data for it
-        var dynamodb = new AWS.DynamoDB();
-        
-        this.docClient = new AWS.DynamoDB.DocumentClient();
-        
-
         var params = {
-            TableName : "properties"
+            TableName : "properties",
+            Key: {
+                'zpid': '109181542'
+            }
         };
-        dynamodb.scan(params, function (err, data) {
+        this.docClient.query(params, function (err, data) {
             if (err) {
                 console.log(err, err.stack);
                 return {}
             } else {
-                console.log('data', data);
+                console.log('data');
                 return data.Items;
             }
         });
